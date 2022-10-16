ea043858c1356ce40610ff3f50b6801c6f754aee
Mathias Brekkan <mathias.brekkan@outlook.com>
2022-10-16 18:53:48 -0400

    Add data format

--- Scripts/barchart.js
+++ Scripts/barchart.js
@@ -2,7 +2,7 @@
 
 
     // -----
-
+    // Format [{actionName, Count}, {actionName, Count}, {actionName, Count}, {actionName, Count}, {actionName, Count}]
 	let segmentationPath1 = "Arms_P1_20_4_6_Prov_Segments.csv";
 	let interactionLogPath1 = "Arms_P1_InteractionsLogs.json";
 
