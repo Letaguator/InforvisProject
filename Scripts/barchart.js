73dfbc39de43dfef1dfb7e4ae08b3e6c889b4e85
Mathias Brekkan <mathias.brekkan@outlook.com>
2022-10-16 18:54:54 -0400

    Remove redundant code

--- Scripts/barchart.js
+++ Scripts/barchart.js
@@ -5,9 +5,4 @@
 	console.log("Arr: ", ds1_UISegmented );
     // -----
     // Format [{actionName, Count}, {actionName, Count}, {actionName, Count}, {actionName, Count}, {actionName, Count}]
-	let segmentationPath1 = "Arms_P1_20_4_6_Prov_Segments.csv";
-	let interactionLogPath1 = "Arms_P1_InteractionsLogs.json";
-
-	let segmentInteractionArr = await readDataAsync(segmentationPath1, interactionLogPath1);
-	console.log("Arr: ", segmentInteractionArr);
 })();
