83ff3c9e0f4258133e283d80c979af5415194384
Mathias Brekkan <mathias.brekkan@outlook.com>
2022-10-16 18:32:21 -0400

    Clean up code

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,6 +1,7 @@
 ;(async function() {
 	// Think_aloud interactions should be ignored in most cases, see Dataset README.txt
-	async function readData(segmentationPath, interactionLog)
+	// Some interactions are outside the segmentations
+	async function readDataAsync(segmentationPath, interactionLog)
 	{
 		const segmentationPathComplete = "Data/Dataset_1/Segmentation/" + segmentationPath;
 		const interactionLogComplete = "Data/Dataset_1/UserInteractions/" + interactionLog;
@@ -19,13 +20,7 @@
 				
 				var segmentInteractionData = csvData.map((segmentData) => {
 					segmentData.interactions = jsonData.filter(interaction => {
-						let interactionEndTime = interaction.time;
-						// Compensate for only Reading and Doc_open interaction types having durations
-						if(interaction.InteractionType === "Reading" || interaction.InteractionType === "Doc_open")
-							interactionEndTime = interaction.time + interaction.duration;
-						// interactionEndTime = interaction.time;
-
-						return (interaction.time >= segmentData.start) && (interactionEndTime < segmentData.end);
+						return (interaction.time >= segmentData.start) && (interaction.time < segmentData.end);
 					});
 					return segmentData;
 				});
@@ -39,7 +34,6 @@
 				});
 				console.log(count)
 				return segmentInteractionData;
-
 			}).catch(function(error) { console.warn(error)} )
 		}).catch(function(error) { console.warn(error)} )
 	}
@@ -50,6 +44,6 @@
 	let segmentationPath1 = "Arms_P1_20_4_6_Prov_Segments.csv";
 	let interactionLogPath1 = "Arms_P1_InteractionsLogs.json";
 
-	let segmentInteractionArr = await readData(segmentationPath1,interactionLogPath1);
-	console.log("Arr: ",segmentInteractionArr);
+	let segmentInteractionArr = await readDataAsync(segmentationPath1, interactionLogPath1);
+	console.log("Arr: ", segmentInteractionArr);
 })();
