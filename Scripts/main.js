5ffab8fa68d253243a8151910e193a68d5b0a211
Mathias Brekkan <mathias.brekkan@outlook.com>
2022-10-16 17:05:33 -0400

    Load raw data, and some refactoring

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,4 +1,17 @@
 ;(function() {
-	
+	function readData(path)
+	{
+		let segmentationPath = "Data/Dataset_1/Segmentation/Arms_P1_20_4_6_Prov_Segments.csv";
+		let interactionLog = "Data/Dataset_1/UserInteractions/Arms_P1_InteractionsLogs.json";
 
+		d3.csv(segmentationPath).then(function(csvData){
+			console.log(csvData)
+			d3.json(interactionLog).then(function(jsonData){
+				console.log(jsonData)
+				
+			}).catch(function(error) {console.warn(error)})
+		}).catch(function(error) {console.warn(error)})
+	}
+
+	readData();
 })();
